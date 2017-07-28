7c840c7456c9f96cf726ffe1b91a1dc15503c554
bananvyhe <loadonden@yahoo.com>
2017-07-28 19:33:38 +0500

    stylesheet separate off vue file in standart position on rails

---
+++ app/javascript/packs/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+ 
